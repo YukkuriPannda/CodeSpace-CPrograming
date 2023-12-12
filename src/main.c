ca361b329ef27c0239258827654abdd8aee30aa8
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-12-12 04:04:20 +0000

    Gate()

--- src/main.c
+++ src/main.c
@@ -2,6 +2,24 @@
 
 int main(void)
 {
-    printf("Hello World!\n");
+
     return 0;
+}
+typedef struct
+{
+    int id;
+    char name[30];
+    char reason[50];
+} PersonCard;
+
+PersonCard Gate(){
+    PersonCard personCard;
+    printf("学籍、職員番号を入力");
+    scanf("%d",&personCard.id);
+    
+    printf("名前を入力");
+    scanf("%s",&personCard.name);
+
+    printf("入校理由を入力");
+    scanf("%s",&personCard.reason);
 }
