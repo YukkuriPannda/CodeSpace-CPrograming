5042b92ae20a8c6069397acac19c8f522876c876
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-12-12 05:00:29 +0000

    kadai1 comp

--- src/main.c
+++ src/main.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
-int main(void)
-{
-    PersonCard personCard;
-    return 0;
-}
 typedef struct
 {
     int id;
     char name[30];
     char reason[50];
 } PersonCard;
+int main(void)
+{
+    PersonCard personCard = {
+        0,"",""
+    };
+    Gate(&personCard);
+    Disp(personCard);
+    return 0;
+}
 
-PersonCard Gate(PersonCard *personCard_ip){
-    printf("学籍、職員番号を入力");
-    scanf("%d",personCard_ip->id);
+void Gate(PersonCard *personCard_ip){
+    printf("学籍、職員番号を入力\n");
+    scanf("%d",&personCard_ip->id);
     
-    printf("名前を入力");
-    scanf("%s",personCard_ip->name);
+    printf("名前を入力\n");
+    scanf("%s",&personCard_ip->name);
 
-    printf("入校理由を入力");
-    scanf("%s",personCard_ip->reason);
+    printf("入校理由を入力\n");
+    scanf("%s",&personCard_ip->reason);
+}
+void Disp(PersonCard personCard_in){
+    printf("id:%d \nname:%s \nreason:%s \n"
+        ,personCard_in.id,personCard_in.name,personCard_in.reason);
 }
