0b71550949e1bbc568b97764293633a1142b129e
YukkuriPannda <100471518+YukkuriPannda@users.noreply.github.com>
2023-12-12 04:37:28 +0000

    baka fix

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-
+    PersonCard personCard;
     return 0;
 }
 typedef struct
@@ -12,14 +12,13 @@ typedef struct
     char reason[50];
 } PersonCard;
 
-PersonCard Gate(){
-    PersonCard personCard;
+PersonCard Gate(PersonCard *personCard_ip){
     printf("学籍、職員番号を入力");
-    scanf("%d",&personCard.id);
+    scanf("%d",personCard_ip->id);
     
     printf("名前を入力");
-    scanf("%s",&personCard.name);
+    scanf("%s",personCard_ip->name);
 
     printf("入校理由を入力");
-    scanf("%s",&personCard.reason);
+    scanf("%s",personCard_ip->reason);
 }
